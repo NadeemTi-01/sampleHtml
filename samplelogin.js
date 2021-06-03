9bb6f68cda369b58ac24146937c355cfd6405651
Nadeem-khan10 <khan.wid28@gmail.com>
2021-06-03 16:35:30 +0530

    Applying CSS class and Some logic changes

--- samplelogin.js
+++ samplelogin.js
@@ -4,16 +4,22 @@ function ShowSecretKeyInput()
 	var secretKeyDisplay = document.getElementById('secretKeyinput').style.display;
 	if (secretKeyDisplay == 'none') {
 		document.getElementById('secretKeyinput').style.display = 'block';
+		document.getElementById('wrongKeyCount').style.display = 'block';
+		//document.getElementById('hideIcon').style.display = 'block';
 		document.getElementById('clickHereText').innerHTML = "Hide";
-	}else
+	}
+	else
 	{
 		document.getElementById('secretKeyinput').style.display = 'none';
 		document.getElementById('ShowSecretKey').style.display = 'none';
 		document.getElementById('isKeyValid').style.display = 'none';
+		document.getElementById('wrongKeyCount').style.display = 'none';
+		//document.getElementById('hideIcon').style.display = 'none';
 		document.getElementById('clickHereText').innerHTML = "Click here to know Login Id and Password";
 		document.getElementById('SecretKeyTxt').value = "";
 	}
 }
+
 var wrongKeyCounter = 2;
 function ValidateSecretKey(){
 	var SecretKey = "Ab1100";
@@ -30,17 +36,24 @@ function ValidateSecretKey(){
 	{
 		document.getElementById('isKeyValid').style.display = 'block';
 		document.getElementById('wrongKeyCount').innerHTML = "Attempt remaining : " + wrongKeyCounter;
-		//if (wrongKeyCounter == 1) {
-		//	document.getElementById('wrongKeyCount').innerHTML = "Last Attempt";
-		//	document.getElementById('isKeyValid').style.background = 'red';
-		//}
+		if (wrongKeyCounter == 2) {
+			document.getElementById('wrongKeyCount').style.background = 'orange';
+			document.getElementById('wrongKeyCount').style.color = 'white';
+		}
+		if (wrongKeyCounter == 1) {
+			//document.getElementById('wrongKeyCount').innerHTML = "Last Attempt";
+			document.getElementById('wrongKeyCount').style.background = 'red';
+			document.getElementById('wrongKeyCount').style.color = 'white';
+		}
 		if (wrongKeyCounter == 0) {
-			document.getElementById('secretKeyinput').style.display = 'none';
 			document.getElementById('isKeyValid').innerHTML = "You exceeded maximum attempt. Refresh page to try again";
+			document.getElementById('loginForm').style.display = 'none';
+			document.getElementById('wrongKeyCount').style.display = 'none';
+			document.getElementById('clickHereText').style.display = 'none';
+			document.getElementById('secretKeyinput').style.display = 'none';
+			document.getElementById('refreshIcon').style.display = 'block';
 			document.getElementById('isKeyValid').style.background = 'red';
 			document.getElementById('isKeyValid').style.color = 'white';
-			document.getElementById('clickHereText').style.display = "none";
-			document.getElementById('loginForm').style.display = 'none';
 		}
 		else
 		{
@@ -57,11 +70,20 @@ function Login(){
 			var userId = document.getElementById('Idtxt').value;
 			var userPass = document.getElementById('Passwordtxt').value;
 
-			if (userId == Id && userPass == pass) {
-				location.href = "sampleMain.html";
+			if (userId != "" && userPass != "") 
+			{
+				if (userId == Id && userPass == pass) 
+				{
+					location.href = "sampleMain.html";
+				}
+				else
+				{
+					alert("Id or Password not correct! Try again");
+				}
 			}
-			else{
-				alert("Id or Password not correct! Try again");
+			else
+			{
+				alert("Id or Password is blank!");
 			}
 		}
 
@@ -72,4 +94,8 @@ function GetName(){
       document.getElementById('greetMsg').innerHTML = greetMsg;
 
       document.getElementById('nameDiv').style.display = 'none';
+}
+
+function ReloadPage(){
+	location.reload();
 }
